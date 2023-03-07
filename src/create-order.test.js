070f680f9e4fbae340e2ecd9ed51383b7c5b04de
Diego Fernandes <diego.schell.f@gmail.com>
2023-03-07 16:01:40 -0300

    add priority test

--- src/create-order.test.js
+++ src/create-order.test.js
@@ -6,8 +6,17 @@ import { createOrder } from './create-order.js'
 test('create new order', async () => {
   const order = await createOrder({
     customerId: 'fake-customer-id',
-    amount: 5000,
+    amount: 1000,
   })
 
   assert.ok(order.id)
+})
+
+test('orders with amount higher than 3000 should me marked as priority', async () => {
+  const order = await createOrder({
+    customerId: 'fake-customer-id',
+    amount: 5000,
+  })
+
+  assert.equal(order.priority, true)
 })
